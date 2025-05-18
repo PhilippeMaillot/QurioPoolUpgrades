69b7e1013c3ed86a3e11a1f4f37d0f68c19f3af6
PhilippeMaillot <maillotphilippe78@gmail.com>
2025-05-18 13:44:35 +0200

    reordered

--- backend/AugPoolDatas/models/Armour.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Armour', {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
-    name: DataTypes.STRING,
-    pool_id: DataTypes.INTEGER,
-    budget: DataTypes.INTEGER
-  }, { timestamps: false });
-};
--- backend/AugPoolDatas/models/Skill.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Skill', {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
-    name: DataTypes.STRING,
-    cost: DataTypes.INTEGER
-  }, { timestamps: false });
-};
